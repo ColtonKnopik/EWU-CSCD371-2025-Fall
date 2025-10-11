d56a27898c1911a72952cb46ba019b6ed4a8c725
ColtonKnopik <ColtonKnopik@gmail.com>
2025-10-11 16:36:43 -0700

    Adds JokeServiceTests

---
+++ CanHazFunny/CanHazFunny.Tests/JokeServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+namespace CanHazFunny.Tests
+{
+    public class JokeServiceTests
+    {
+        [Fact]
+        public void JokeService_GetJoke_ReturnsNonEmptyString()
+        {
+            // Arrange
+            var jokeService = new JokeService();
+            // Act
+            string joke = jokeService.GetJoke();
+            // Assert
+            Assert.False(string.IsNullOrWhiteSpace(joke));
+        }
+    }
+}
