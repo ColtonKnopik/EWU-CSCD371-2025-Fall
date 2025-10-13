aee3682ad61d55a327f2da513cdeb94deb0dfecf
Chloe Stanton <cstanton5@ewu.edu>
2025-10-13 00:02:25 -0700

    Removed unecessary comments in Program.cs

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -4,8 +4,6 @@ sealed class Program
 {
     static void Main(string[] args)
     {
-        //Feel free to use your own setup here - this is just provided as an example
-        //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
         IOutput output = new ConsoleOutput();
         IJokeService jokeService = new JokeService();
         Jester jester = new Jester(output, jokeService);
